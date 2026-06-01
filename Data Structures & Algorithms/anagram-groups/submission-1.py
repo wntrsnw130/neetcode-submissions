6c949cc23f60b4a7a51132098ef74630f993cc74
wntrsnw130 <w1nt3rsn0wo6o4@gmail.com>
2026-06-02 00:52:13 +0800

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        ans = collections.defaultdict(list)
+
+        for s in strs:
+            key = "".join(sorted(s))
+            ans[key].append(s)
+        
+        return list(ans.values())
+
+        
